87cb7365c01609ec29e414cc040d3eb05a873c76
Jill <yul.guseva004@gmail.com>
2026-09-13 13:29:52 +0300

    3.1.5_bookstore_done

--- bookstore/src/main/java/bookstore/trainingproject/BookstoreApplication.java
+++ bookstore/src/main/java/bookstore/trainingproject/BookstoreApplication.java
@@ -1,13 +1,42 @@
 package bookstore.trainingproject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
-@SpringBootApplication (scanBasePackages = "bookstore")
+import bookstore.domain.Book;
+import bookstore.domain.BookRepository;
+
+@SpringBootApplication(scanBasePackages = "bookstore")
+@EntityScan(basePackages = "bookstore.domain")
+@EnableJpaRepositories(basePackages = "bookstore.domain")
 public class BookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner dataLoader(BookRepository repository) {
+        return args -> {
+            repository.save(new Book(
+                "Harry Potter",
+                "J.K. Rowling",
+                1997,
+                "123456",
+                19.99
+            ));
+
+            repository.save(new Book(
+                "The Hobbit",
+                "J.R.R. Tolkien",
+                1937,
+                "654321",
+                15.99
+            ));
+        };
+    }
 }
--- bookstore/src/main/java/bookstore/web/BookController.java
+++ bookstore/src/main/java/bookstore/web/BookController.java
@@ -7,12 +7,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import bookstore.domain.Book;
+import bookstore.domain.BookRepository;
 
 @Controller 
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String showIndex(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "index"; // index.html
     }
     @GetMapping("/one")
