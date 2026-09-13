efc34c9aa8855e58dd811f90d0cd9d34cfad0a0f
Jill <yul.guseva004@gmail.com>
2026-09-13 14:57:33 +0300

    3.3_bookstore_CRUD_done

--- bookstore/src/main/java/bookstore/web/BookController.java
+++ bookstore/src/main/java/bookstore/web/BookController.java
@@ -1,10 +1,10 @@
-//Add a new controller called BookController which handle get request to the path /index
-
 package bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
@@ -22,5 +22,33 @@ public class BookController {
     public String showIndex(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "Bookstore";
+    }
+
+    @GetMapping("/add")
+    public String addBook(Model model){
+        model.addAttribute("book", new Book());
+        return "addBook";
+    }
+
+    @PostMapping("/save")
+    public String save(Book book){
+        if (hasBookData(book)) {
+            bookRepository.save(book);
+        }
+        return "redirect:/bookstore";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/bookstore";
+    }
+
+    private boolean hasBookData(Book book) {
+        return (book.getTitle() != null && !book.getTitle().isBlank())
+                || (book.getAuthor() != null && !book.getAuthor().isBlank())
+                || book.getPublicationYear() != null
+                || (book.getIsbn() != null && !book.getIsbn().isBlank())
+                || book.getPrice() != null;
     }
 }
